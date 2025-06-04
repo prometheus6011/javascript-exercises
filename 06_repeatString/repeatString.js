9b34dd61840ff17d58735a9dec352f7b606ca1c8
prometheus6011 <mahidhar1287@gmail.com>
2025-06-04 17:59:08 -0400

    Completed Exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, i) {
+  if (i < 0) {
+    return "ERROR";
+  }
+  let res = "";
+  for (let iter = 0; iter < i; ++iter) {
+    res += str;
+  }
+  return res;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  const res = [];
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      res.push(item);
+    }
+  });
+
+  return res;
 };
 
 // Do not edit below this line
